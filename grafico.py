9de2a8b6925042d233d8fa5da095ef8699bd558c
laiamp <81615713+laiamp@users.noreply.github.com>
2023-06-08 17:41:27 +0200

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -12,11 +12,14 @@ class Grafico():
         plt.legend(self.labels, loc="best")
         plt.axis('equal')
 
-    def get_porcentaje(self, dict): #convierte los totales de cada de cada categoría/proveedor del diccionario en porcentajes
-        total = sum(dict.values()) #obtengo la suma de todos los totales
-        dict = {x: dict[x]/total*100 for x in dict} #cambio cada total por el porcentaje correspondiente
-        return dict
 
-    def mostrar(self, fila, columna): #muestra el gráfico en el popup
+    def get_porcentaje(self, dict): 
+        '''convierte los totales de cada de cada categoría/proveedor del diccionario en porcentajes'''
+        total = sum(dict.values())
+        return {x: dict[x]/total*100 for x in dict}
+
+
+    def mostrar(self, fila, columna): 
+        '''muestra el gráfico en el popup'''
         canvas = FigureCanvasTkAgg(self.fig, master=self.popup) #interficie entre la Figura de Matplotlib y canvas de Tkinter
         canvas.get_tk_widget().grid(row=fila, column=columna)
