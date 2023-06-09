3e09d033ae8aa50cb39f581c1ea09365da3a2452
laiamp <81615713+laiamp@users.noreply.github.com>
2023-06-09 14:17:49 +0200

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -9,8 +9,9 @@ class SQLite():
         self.cursor.execute("CREATE TABLE IF NOT EXISTS historial(fecha text PRIMARY KEY, mes text, referencia text, nombre text, proveedor text, precio_unidad real, cantidad real, descuento real, total real, categoria text)")
         self.con.commit() #guarda los cambios
 
+
     def guardar_elemento(self, nombre_tabla, fecha=None, referencia=None, nombre=None, proveedor=None, precio_unidad=None, cantidad=None, descuento=None, categoria=None, stock_min=None, estatus=None):
-        if nombre_tabla == "stock": #guardar elementos en stock
+        if nombre_tabla == "stock":
             data = (referencia, nombre, cantidad, stock_min, categoria, estatus)
             self.cursor.execute(f"INSERT INTO {nombre_tabla}(referencia, nombre, cantidad, stock_min, categoria, estatus) VALUES (?,?,?,?,?,?)", data)
 
@@ -25,28 +26,36 @@ class SQLite():
             self.cursor.execute(f"INSERT INTO {nombre_tabla} (proveedor) VALUES(?)", (proveedor,))
         self.con.commit()
 
-    def get_tabla(self, nombre_tabla): #obtiene todos los valores de una tabla
+
+    def get_tabla(self, nombre_tabla): 
+        '''obtiene todos los valores de una tabla'''
         if nombre_tabla != "stock":
             self.cursor.execute(f"SELECT * FROM {nombre_tabla}")
         else:
             self.cursor.execute("SELECT referencia, nombre, cantidad, categoria, estatus FROM stock")
         return self.cursor.fetchall()
 
-    def get_meses(self): #obtiene todos los meses en los que se haya añadido algún producto
-        self.cursor.execute("SELECT mes FROM historial") #algunos meses pueden estar repetidos
-        lista = self.cursor.fetchall()
-        lista = list(dict.fromkeys(lista)) #elimino duplicados conviertiendo la lista en un diccionario
-        return lista
 
-    def get_descarga(self, id): #obtiene todos los parámetros (menos el mes y el año) pertenecientes al mes seleccionado
+    def get_meses(self): 
+        '''obtiene todos los meses en los que se haya añadido algún producto'''
+        self.cursor.execute("SELECT mes FROM historial") # algunos meses pueden estar repetidos
+        return list(set(self.cursor.fetchall())) # eliminamos duplicados
+
+
+    def get_descarga(self, id): 
+        '''obtiene todos los parámetros (menos el mes y el año) pertenecientes al mes seleccionado'''
         self.cursor.execute("SELECT fecha, referencia, nombre, proveedor, precio_unidad, cantidad, descuento, total, categoria from historial WHERE mes = ?", (id,))
         return self.cursor.fetchall()
 
-    def get_editar(self, id): #obtener valores de stock/historial para editar
+
+    def get_editar(self, id): 
+        '''obtiene valores de stock/historial para editar'''
         self.cursor.execute("SELECT proveedor, precio_unidad, descuento, total, cantidad FROM historial WHERE fecha=?", (id,))
         return self.cursor.fetchall()
 
-    def get_elemento_tabla(self, nombre_tabla, id, elemento): #obtiene un elemento específico de stock en base a su referencia
+
+    def get_elemento_tabla(self, nombre_tabla, id, elemento):
+        '''obtiene un elemento específico de stock en base a su referencia'''
         if nombre_tabla == "stock":
             self.cursor.execute(f"SELECT {elemento} FROM stock WHERE referencia =?", (id,))
 
@@ -55,25 +64,31 @@ class SQLite():
 
         return self.cursor.fetchone()[0]
 
-    def get_total(self, mes, columna): #obtiene un diccionario que agrupe cada categoría/proveedor con su gasto correspondiente
+ 
+    def get_total(self, mes, columna):
+        '''obtiene un diccionario que agrupe cada categoría/proveedor con su gasto correspondiente'''
         self.cursor.execute(f"SELECT {columna}, total FROM historial WHERE mes = ?", (mes,))
         lista = self.cursor.fetchall()
         dictionary = {} #creo un diccionario vacío
         for i in lista:
             #i[0] es la categoría del producto e i[1] es su gasto total
-            if i[0] in dictionary: #si la categoría ya la habíamos encontrado
-                 dictionary[i[0]] += i[1] #se suma el gasto de ese producto al gasto total de la categoría
+            if i[0] in dictionary: # si la categoría ya la habíamos encontrado
+                 dictionary[i[0]] += i[1] # se suma el gasto de ese producto al gasto total de la categoría
 
-            else: #si la categoría no estaba aún se crea un nuevo par en dictionary con la categoría y el gasto del producto
+            else: # si la categoría no estaba aún se crea un nuevo par en dictionary con la categoría y el gasto del producto
                 dictionary.update({i[0]: i[1]})
 
         return dictionary
 
-    def borrar_elemento(self, nombre_tabla, columna_id, id): #elimina un elemento de una tabla
+
+    def borrar_elemento(self, nombre_tabla, columna_id, id): 
+        '''elimina un elemento de una tabla'''
         self.cursor.execute(f"DELETE FROM {nombre_tabla} WHERE {columna_id} = ?", (id,))
         self.con.commit()
 
-    def editar_from_stock(self, nombre_tabla, id, columna1=None, columna2=None, columna3=None, elemento1=None, elemento2=None, elemento3=None): #editar nombre, categoria y/o stock_mín desde el frame stock
+
+    def editar_from_stock(self, nombre_tabla, id, columna1=None, columna2=None, columna3=None, elemento1=None, elemento2=None, elemento3=None):
+        '''editar nombre, categoria y/o stock_mín desde el frame stock'''
         if columna2 is None and columna3 is None: #1 elemento a editar
             self.cursor.execute(f"UPDATE {nombre_tabla} SET {columna1} = ? WHERE referencia = ?", (elemento1, id))
 
@@ -85,30 +100,37 @@ class SQLite():
 
         self.con.commit()
 
-    def editar_from_historial(self, id, datos): #edita proveedor precio descuento y total de historial
+
+    def editar_from_historial(self, id, datos):
+        '''edita proveedor precio descuento y total de historial'''
         self.cursor.execute("UPDATE historial SET proveedor=?, precio_unidad=?, descuento=?, total=? WHERE fecha=?", datos + (id,))
         self.con.commit()
 
-    def existe(self, nombre_tabla, columna, elemento): #comprueba la existencia de un elemento concreto en una tabla concreta
+
+    def existe(self, nombre_tabla, columna, elemento):
+        '''comprueba la existencia de un elemento concreto en una tabla concreta'''
         self.cursor.execute(f"SELECT {columna} FROM {nombre_tabla} WHERE {columna} =?", (elemento,))
         lista = self.cursor.fetchall() #lista con todos los elementos de la tabla que correspondan con el elemento escogido
-        if len(lista) != 0: #si la lista contiene elementos se retorna True
-            return True
-        else: #si la lista está vacía significa que ese elemento no aparece y se retorna False
-            return False
+        return len(lista) != 0
 
-    def set_valor_producto(self, nombre_tabla, columna, id, valor): #cambia el valor de un único parámetro de un producto concreto en base a su id
+
+    def set_valor_producto(self, nombre_tabla, columna, id, valor):
+        '''cambia el valor de un único parámetro de un producto concreto en base a su id'''
         if nombre_tabla == "stock":
             self.cursor.execute(f"UPDATE stock SET {columna}=? WHERE referencia=?", (valor, id))
         else:
             self.cursor.execute(f"UPDATE historial SET {columna}=? WHERE fecha=?", (valor, id))
         self.con.commit()
 
-    def set_valor_tabla(self, nombre_tabla, columna, nuevo, old): #cambia un único valor todas las veces que aparezca en una tabla
+
+    def set_valor_tabla(self, nombre_tabla, columna, nuevo, old): 
+        '''cambia un único valor todas las veces que aparezca en una tabla'''
         self.cursor.execute(f"UPDATE {nombre_tabla} SET {columna} = ? WHERE {columna} = ?", (nuevo, old))
         self.con.commit()
 
-    def filtrar(self, nombre_tabla, columna1, criterio1, columna2=None, criterio2=None, columna3=None, criterio3=None): #selecciona los productos/compras que cumplan con X criterios
+
+    def filtrar(self, nombre_tabla, columna1, criterio1, columna2=None, criterio2=None, columna3=None, criterio3=None):
+        '''selecciona los productos/compras que cumplan con X criterios'''
         if nombre_tabla == "historial":
             if columna2 is None and columna3 is None: #1 elemento seleccionado como criterio
                 self.cursor.execute(f"SELECT * FROM historial WHERE {columna1} = ?", (criterio1,))
