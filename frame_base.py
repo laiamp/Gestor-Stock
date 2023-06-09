673dbed7a3e39772d62df8ecbebe625c3e17b31b
laiamp <81615713+laiamp@users.noreply.github.com>
2023-06-09 14:20:19 +0200

    Update frame_base.py

--- frame_base.py
+++ frame_base.py
@@ -6,12 +6,14 @@ class Frame_Base(Frame):
         self.screen_height = self.winfo_screenheight()
         self._popup_abierto = False
 
-    def crear_popup(self): #abre un popup e indica que hay un popup abierto
+    def crear_popup(self):
+        '''abre un popup e indica que hay un popup abierto'''
         self._popup_abierto = True
         self.popup = Toplevel()
         self.popup.wm_protocol("WM_DELETE_WINDOW", self.quitar_popup) #si se cierra el popup se llama al método quitar_popup
         self.popup.transient(self) #superpone el popup
 
-    def quitar_popup(self): #elimina el popup abierto e indica que no hay ningún popup abierto
+    def quitar_popup(self):
+        '''elimina el popup abierto e indica que no hay ningún popup abierto'''
         self._popup_abierto = False
         self.popup.destroy()
